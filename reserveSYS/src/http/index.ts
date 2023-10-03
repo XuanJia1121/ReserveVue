45076dc8243a177d6a47b516f7e32399ed53d6ad
xuanjia <xuanjia961121@gmail.com>
2023-10-03 17:27:52 +0800

    fix

--- reserveSYS/src/http/index.ts
+++ reserveSYS/src/http/index.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from 'axios'
 
 const getApiClient = (options = {}): AxiosInstance => {
   return axios.create({
-    baseURL: '/test',
+    baseURL: 'http://localhost:8081/api',
     headers: {
       'Content-type': 'application/json',
        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
